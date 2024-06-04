effd7fd582a533fe1e02c361ed4e1430a18413fe
keeganthewhi <139489326+keeganthewhi@users.noreply.github.com>
2024-06-04 17:55:54 +0300

    Bitim

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,30 @@
-export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
-
-export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_PLUS = "MEMORY_PLUS";
+export const TOTAL_ADD_MEMORY = "TOTAL_ADD_MEMORY";
+export const CLEAR_MEMORY = "CLEAR_MEMORY";
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const changeOperation = (operator) => {
+  return { type: CHANGE_OPERATION, payload: operator };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY, payload: 0 };
+};
+
+export const memoryPlus = () => {
+  return { type: MEMORY_PLUS };
+};
+
+export const totalAddMemory = () => {
+  return { type: TOTAL_ADD_MEMORY };
+};
+
+export const clearMemory = () => {
+  return { type: CLEAR_MEMORY, payload: 0 };
+};
